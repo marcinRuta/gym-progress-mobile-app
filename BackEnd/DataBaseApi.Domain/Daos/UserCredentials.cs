b28a8a84921aafc0a1730893728e108360764c17
Marcin Ruta <m.ruta@stud.elka.pw.edu.pl>
2021-08-24 14:20:14 +0200

    Little changes to databseApi

---
+++ BackEnd/DataBaseApi.Domain/Daos/UserCredentials.cs
@@ -0,0 +1,27 @@
+﻿using DataBaseApi.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseApi.Domain.Daos
+{
+    public class UserCredentials : Entity
+    {
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public User UserDetails { get; private set; }
+
+        public UserCredentials(int id, string username, string password) : base(id)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public void AddUser(User user)
+        {
+            this.UserDetails = user;
+        }
+    }
+}
